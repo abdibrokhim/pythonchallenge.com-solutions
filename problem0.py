0f8f24b788b89f059d94a390009efa21ea8f5a22
abdibrokhim <abdibrokhim@gmail.com>
2022-12-04 01:52:15 +0500

    init

---
+++ problem0.py
@@ -0,0 +1,9 @@
+print(pow(2, 38))
+
+# http://www.pythonchallenge.com/pc/def/0.html
+
+# in the above url, try to replace 0 with answer
+
+# you will get something like this
+
+# http://www.pythonchallenge.com/pc/def/274877906944.html
---
+++ problem1.py
@@ -0,0 +1,42 @@
+string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+def main(s: str) -> str:
+    new_s = ""
+    for i in s:
+        if i == "y":
+            new_s += "a"
+        elif i == "z":
+            new_s += "b"
+        elif ord(i) >= 97 and ord(i) <= 120:
+            new_s += chr(ord(i) + 2)
+        else:
+            new_s += i
+    return new_s
+
+print(main(string))
+
+print("\n")
+
+# with built-in methods
+def main1(s: str) -> str:
+    l = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+    return s.translate(l)
+
+print(main1(string))
+
+print("\n")
+
+# now apply to url
+print(main("map"))
+
+
+# http://www.pythonchallenge.com/pc/def/map.html
+
+# in the above url, try to replace map with answer
+
+# you will get something like this
+
+# http://www.pythonchallenge.com/pc/def/ocr.html
+
+
+
